79c562eeee6c64bb326553d5b10b361af68e3dbb
Thien <thien@hoasen.io>
2025-09-23 19:49:09 +0900

    fix build

--- src/app/box.tsx
+++ src/app/box.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LayoutBox } from '../layout/box'
-import '@radix-ui/themes/styles.css'
 import '../style/main.css'
 
 createRoot(document.getElementById('root')!).render(
