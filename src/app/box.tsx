9eef1186a3f348534372eeaaec67fe2d5c3545c8
Thien <thien@hoasen.io>
2025-09-30 17:51:28 +0900

    store timezones to local storage

--- src/app/box.tsx
+++ src/app/box.tsx
@@ -2,17 +2,28 @@ import type { ReactElement } from 'react'
 import { useState } from 'react'
 import { LayoutBox } from '../layout/box'
 import { PlaceBox } from '../place/box'
-import { MOCK_TIMEZONES } from '../place/type'
 import { TimeBox } from '../time/box'
 
 export function AppBox(): ReactElement {
   const [time, setTime] = useState(initTime)
-  const [timezones, setTimezones] = useState(MOCK_TIMEZONES)
+  const [timezones, setTimezones] = useState<string[]>(() => {
+    const tzs = localStorage.getItem('timezones') as string | null
+    return tzs ? JSON.parse(tzs) : []
+  })
+
+  const onTimezonesChange = (tzs: string[]) => {
+    setTimezones(tzs)
+    localStorage.setItem('timezones', JSON.stringify(tzs))
+  }
 
   return (
     <LayoutBox>
       <div className="p-12 h-full flex flex-col gap-12">
-        <PlaceBox time={time} timezones={timezones} setTimezones={setTimezones} />
+        <PlaceBox
+          time={time}
+          timezones={timezones}
+          setTimezones={onTimezonesChange}
+        />
         <TimeBox time={time} setTime={setTime} />
       </div>
     </LayoutBox>
--- src/place/type.ts
+++ src/place/type.ts
@@ -4,13 +4,4 @@ export interface Place {
   isPrimary: boolean
 }
 
-export const MOCK_TIMEZONES: string[] = [
-  'America/New_York',
-  'Asia/Ho_Chi_Minh',
-  'Asia/Tokyo',
-  'Europe/London',
-  'Europe/Rome',
-  'Europe/Paris',
-]
-
 export const LOCAL_TIMEZONE = Intl.DateTimeFormat().resolvedOptions().timeZone
